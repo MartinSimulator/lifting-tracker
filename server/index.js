015d8b186686571384e40a3fc2f33d8743e56c8c
MartinSimulator <martin.simulator@gmail.com>
2026-01-04 21:52:54 -0800

    updated index.js to use Render API for cors

--- server/index.js
+++ server/index.js
@@ -1,31 +1,37 @@
 // load vars from .env
-require('dotenv').config();
+require("dotenv").config();
 
 // import libraries express, mongoose, and cors
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 // create server instance where app is the backend application object
-const app = express()
+const app = express();
 // import routes
-const workoutRoutes = require('./routes/workouts');
+const workoutRoutes = require("./routes/workouts");
 
 // allow frontend to talk to us
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://lifting-tracker-nu.vercel.app/", // Your actual Vercel URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 // allow us to read expression sent in request body
 app.use(express.json());
 // use the workout routes
-app.use('/api/workouts', workoutRoutes);
-
+app.use("/api/workouts", workoutRoutes);
 
 // connect to mongodb database
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('MongoDB connection error', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error", err));
 
 // test to make sure server works
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 // start the server
